737110f55983d3bbfb6b337c88228ab06641a8cb
Heena Tabasum <heena.tabassum@ideas.com>
2020-07-24 00:34:20 +0530

    Student module lazy loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,8 @@ import { HomeComponent } from './home/home.component';
 const routes: Routes = [
   {path: '', redirectTo: 'home', pathMatch: 'full'},
   {path: 'home', component: HomeComponent},
-  {path: 'lazy', loadChildren: './lazy/lazy.module#LazyModule'}
+  {path: 'lazy', loadChildren: './lazy/lazy.module#LazyModule'},
+  {path: 'student', loadChildren: './student/student.module#StudentModule'}
 ];
 
 @NgModule({
